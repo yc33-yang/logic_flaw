a3397565c7bdb29e7191326c5fe05b8db6141519
yc33-yang <42262512+yc33-yang@users.noreply.github.com>
2018-08-13 15:23:35 +0900

    Update: vector<int> to template vector<T>

--- quick_sort.cc
+++ quick_sort.cc
@@ -1,34 +1,38 @@
 
 #include <vector>
 
-std::vector<int> FilterLessThan(std::vector<int> V, int pivot) {
-	std::vector<int> result;
 
-	 for (int elem : V) {
-		 if (elem < pivot) {
-			 result.push_back(elem);
-		 }
-	 }
+template <class T>
+std::vector<T> FilterLessThan(const std::vector<T> & V, T pivot) {
+	std::vector<T> result;
 
-	 return result;
- }
+	for (T elem : V) {
+		if (elem < pivot) {
+			result.push_back(elem);
+		}
+	
+	}
+	return result;
+}
 
- std::vector<int> FilterGreaterThan(std::vector<int> V, int pivot) {
-	 std::vector<int> result;
 
-	 for (int elem : V) {
-		 if (elem > pivot) {
-			 result.push_back(elem);
-		 }
-	 }
+template <class T>
+std::vector<T> FilterGreaterThan(const std::vector<T> & V, T pivot) {
+	std::vector<T> result;
 
-	 return result;
- }
+	for (T elem : V) {
+		if (elem > pivot) {
+			result.push_back(elem);
+		}
+	}
+	return result;
+}
 
-size_t CountSame(const std::vector<int> &V, int value) {
+template <class T>
+size_t CountSame(const std::vector<T> &V, T value) {
 	size_t count = 0;
 
-	for (int elem : V) {
+	for (T elem : V) {
 		if (elem == value) {
 			count++;
 		}
@@ -36,28 +40,40 @@ size_t CountSame(const std::vector<int> &V, int value) {
 	return count;
 }
 
- std::vector<int> QuickSort(std::vector<int> V) {
-	 if (static_cast<int>(V.size()) <= 1) {
-		 return V;
-	 }
+template <class T>
+std::vector<T> QuickSort(std::vector<T> V) {
+	if (static_cast<T>(V.size()) <= 1) {
+		return V;
+	}
+	
+	T pivot = V.back();
+	size_t num_pivot = CountSame(V, pivot);
+	V.pop_back();
+	
+	std::vector<T> result;
+	for (T elem : QuickSort(FilterLessThan(V, pivot))) {
+		result.push_back(elem);
+	}
+	for (size_t i = 0; i < num_pivot; i++) {
+		result.push_back(pivot);
+	}
+	for (T elem : QuickSort(FilterGreaterThan(V, pivot))) {
+		result.push_back(elem);
+	}
+	
+	return result;
+}
 
-	 int pivot = V.back();
-	 size_t num_pivot = CountSame(V, pivot);	 
-	 V.pop_back();
-
-	 std::vector<int> result;
-	 for (int elem : QuickSort(FilterLessThan(V, pivot))) {
-		 result.push_back(elem);
-	 }
-	 for (size_t i = 0; i < num_pivot; i++) {
-		 result.push_back(pivot);
-	 }
-	 for (int elem : QuickSort(FilterGreaterThan(V, pivot))) {
-		 result.push_back(elem);
-	 }
-
-	 return result;
- }
+template <class T>
+std::ostream & operator << (std::ostream &os, const std::vector<T> &v)
+{
+	size_t num_ret = v.size();
+	for (size_t i = 0; i < num_ret; i++)
+	{
+		os << "[" << i << "] = " << v[i] << '\n';
+	}
+	return os;
+}
 
 
 #include <iostream>
