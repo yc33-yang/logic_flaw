3f2badb6ad510bbebd8a5858d585ee3f98dc3a6c
yc33-yang <42262512+yc33-yang@users.noreply.github.com>
2018-08-13 15:54:34 +0900

    Fix

--- counting_sort.cc
+++ counting_sort.cc
@@ -1,7 +1,9 @@
 #include <vector>
 
+const size_t MAX_INT_VALUE_FOR_SORT = 100; // range 0...100
+
 std::vector<int> CountingSort(std::vector<int> V) {
-	std::vector<int> counting_array(V.size() + 10, 0);
+	std::vector<int> counting_array(1 + MAX_INT_VALUE_FOR_SORT, 0);
 	for (int i = 0; i < static_cast<int>(V.size()); ++i) {
 		++counting_array[V[i]];
 	}
