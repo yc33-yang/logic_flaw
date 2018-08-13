d7c898a56eda6b719c257b616e8177e7e0e34412
yc33-yang <42262512+yc33-yang@users.noreply.github.com>
2018-08-13 15:16:53 +0900

    Add: test driver and int vector printer

--- quick_sort.cc
+++ quick_sort.cc
@@ -44,3 +44,25 @@ std::vector<int> FilterLessThan(std::vector<int> V, int pivot) {
 
 	 return result;
  }
+
+
+template <class T>
+std::ostream & operator << (std::ostream &os, const std::vector<T> &v)
+{
+	size_t num_ret = v.size();
+	for (size_t i = 0; i < num_ret; i++)
+	{
+		os << "[" << i << "] = " << v[i] << '\n';
+	}
+	return os;
+}
+
+ int main()
+ {
+	 std::vector<int> ary = { 33, 4, 1, 92, 33 ,100, 3, 4, 0, 5, 33 };
+	 std::vector<int> ret = QuickSort(ary);
+
+	 std::cout << ret;
+	 
+	 return 0;
+ }
